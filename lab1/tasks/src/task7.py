5645d6c65eec191ecb1586f8433453ab6f5bf090
Seztor <56021991+Seztor@users.noreply.github.com>
2024-10-07 18:59:20 +0300

    Add files via upload

--- lab1/tasks/src/task7.py
+++ lab1/tasks/src/task7.py
@@ -3,8 +3,14 @@ def task7():
     f2 = open('output.txt', 'w')
     n = int(f.readline())
     arr = [(float(item),ind+1) for ind,item in enumerate(f.readline().split())]
-
-    arr.sort()
+    
+    for j in range(1, len(arr)):
+        i = j-1
+        k = arr[j]
+        while arr[i][0] > k[0] and i >= 0:
+            arr[i+1] = arr[i]
+            i = i-1
+        arr[i+1] = k
 
     print(arr[0][1], arr[n//2][1], arr[-1][1], file=f2)
 
