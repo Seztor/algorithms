4536c26390a9bae80f7e378e952572d0f809b085
Pavel <pavel.govorov.06@mail.ru>
2024-10-30 00:32:15 +0300

    clear prints

--- lab2/task7/tests/test_task7.py
+++ lab2/task7/tests/test_task7.py
@@ -11,14 +11,11 @@ def test_should_testing_task7():
     n = 10**5
     arr = [random.randint(-10**2, 10 ** 2) for _ in range(n)]
     write_data(PATH_INPUT, n, arr)
-    print()
-    print(n)
-    #print(arr)
+
     #when
     task7()
-
     ans1, ans2 = read_data(PATH_OUTPUT)
-    print(ans1)
+
     #then
     assert ans1 == ans2
 
