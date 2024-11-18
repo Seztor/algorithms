13b6c19183979b2547c87957fc38a41d3bbf8b20
Pavel <pavel.govorov.06@mail.ru>
2024-11-18 18:29:11 +0300

    run all tests all lab

--- run_all_lab_tests.py
+++ run_all_lab_tests.py
@@ -5,7 +5,8 @@ list_tasks = [1,2,3]
 
 for i in list_tasks:
     os.chdir(f'lab{i}/')
-    print(f'TESTS LAB #{i}')
+
+    print(f'\033[34mTESTS LAB #{i}\033[0m')
     print()
     path_test = f'run_tests_lab{i}.py'
     result = subprocess.run(['python',path_test], capture_output=True, text=True).stdout
