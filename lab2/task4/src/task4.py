18bc9e58d557d176f910e14ff3dee2025dfbc177
Pavel <pavel.govorov.06@mail.ru>
2024-12-01 15:22:08 +0300

    create files runners

--- lab2/task4/src/task4.py
+++ lab2/task4/src/task4.py
@@ -3,7 +3,7 @@ from lab2.utils_lab2 import read_data,write_data
 PATH_INPUT = "../txtf/input.txt"
 PATH_OUTPUT = "../txtf/output.txt"
 
-def bs(arr, key):
+def bin_search(arr, key):
     lt = -1
     rt = len(arr)
     while rt - lt > 1:
@@ -22,7 +22,7 @@ def bs(arr, key):
 
 def task4():
     n, arr_n, k, arr_k = read_data(PATH_INPUT)
-    ans_arr = [bs(arr_n,el) for el in arr_k]
+    ans_arr = [bin_search(arr_n, el) for el in arr_k]
     write_data(PATH_OUTPUT, ans_arr)
     return ans_arr
 
--- run_all_lab_files.py
+++ run_all_lab_files.py
@@ -6,9 +6,9 @@ lab_tasks = [1,2,3]
 for i in lab_tasks:
     os.chdir(f'lab{i}/')
 
-    print(f'\033[34mTESTS LAB #{i}\033[0m')
+    print(f'\033[34mRUNNING LAB #{i}\033[0m')
     print()
-    path_test = f'run_tests_lab{i}.py'
+    path_test = f'run_files_lab{i}.py'
     result = subprocess.run(['python',path_test], capture_output=True, text=True).stdout
     print(result)
     os.chdir(f'..')
